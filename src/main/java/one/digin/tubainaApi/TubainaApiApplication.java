4dcae7a0803630837b92a3f43ca9cc5f4da8f1c7
Debora <debvm@hotmail.com>
2021-07-19 19:15:59 -0300

    initial commit

---
+++ src/main/java/one/digin/tubainaApi/TubainaApiApplication.java
@@ -0,0 +1,13 @@
+package one.digin.tubainaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubainaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubainaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digin/tubainaApi/TubainaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digin.tubainaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubainaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
