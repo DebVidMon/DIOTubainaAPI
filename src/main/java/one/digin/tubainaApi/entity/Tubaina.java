48b56a2039f08134cc3da052cdeaaff4c445f628
Debora <debvm@hotmail.com>
2021-07-19 19:44:44 -0300

    feat(entity): Tubaina

---
+++ src/main/java/one/digin/tubainaApi/entity/Tubaina.java
@@ -0,0 +1,35 @@
+package one.digin.tubainaApi.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.*;
+import lombok.*;
+import one.digin.tubainaApi.enums.TubainaType;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tubaina {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TubainaType type;
+
+}
